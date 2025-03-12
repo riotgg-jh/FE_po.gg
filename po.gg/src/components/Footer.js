d51561a709dc2f264ae713ef92b8c1902355d90f
Mynamisboseong <wow456869@gmail.com>
2025-03-12 20:52:56 +0900

    메인화면 완료

--- po.gg/src/components/Footer.js
+++ po.gg/src/components/Footer.js
@@ -4,10 +4,6 @@ function Footer() {
   return (
     <footer className="footer">
       <p>© 2025 FE_PO.GG. All rights reserved.</p>
-      <p>
-        <a href="/">Home</a> |<a href="/find_duo">Find Duo</a> |
-        <a href="/user_search">User Search</a>
-      </p>
     </footer>
   )
 }
--- po.gg/src/pages/Home.js
+++ po.gg/src/pages/Home.js
@@ -1,11 +1,28 @@
-import "./../styles/home.css"
+import React from "react"
+import "../styles/home.css" // ✅ 스타일 추가
+import logo from "../assets/po.gg logo.png" // ✅ 로고 이미지 추가
 
-function home() {
+function Home() {
   return (
     <div className="home-container">
-      <h1>메인페이지 화면입니다.</h1>
+      {/* 로고 */}
+      <img src={logo} alt="PO.GG Logo" className="home-logo" />
+
+      {/* 검색 바 */}
+      <div className="search-container">
+        <select className="region-select">
+          <option value="KR">KR</option>
+          <option value="NA">개발 중...</option>
+        </select>
+        <input
+          type="text"
+          placeholder="소환사명을 입력하세요..."
+          className="search-input"
+        />
+        <button className="search-button">🔍</button>
+      </div>
     </div>
   )
 }
 
-export default home
+export default Home
