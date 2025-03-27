54a573da93046ba0b786dddeb82514bd8f41a379
Mynamisboseong <wow456869@gmail.com>
2025-03-28 00:18:23 +0900

    매치카드 아군&적군 정보, 유저프로필 랭크정보 수정

--- po.gg/src/components/user/UserProfile.js
+++ po.gg/src/components/user/UserProfile.js
@@ -2,7 +2,7 @@ import React from "react"
 import "../../styles/user_search.css"
 import RankInfo from "./RankInfo"
 
-const UserProfile = ({ user }) => {
+const UserProfile = ({ user, onRefresh }) => {
   return (
     <div className="user-profile">
       {/* 왼쪽: 프로필 정보 */}
@@ -17,12 +17,17 @@ const UserProfile = ({ user }) => {
             {user.summonerName}#{user.tagLine}
           </h2>
           <p className="profile-level">Level: {user.level}</p>
-          <button className="refresh-button">전적갱신</button>
+          <button
+            className="refresh-button"
+            onClick={() => onRefresh(user.summonerName, user.tagLine)}
+          >
+            전적갱신
+          </button>
         </div>
       </div>
 
-      {/* 오른쪽: 티어 정보 (UserProfile 내부로 이동) */}
-      <RankInfo rank={user.rank} />
+      {/* 오른쪽: 티어 정보 (soloEntries와 flexEntries 전달) */}
+      <RankInfo soloEntries={user.soloEntries} flexEntries={user.flexEntries} />
     </div>
   )
 }
